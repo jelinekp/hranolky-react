b2bfe9debacdec037352ae1829ffb6bca696ef79
jelinekp <jelinekp6@gmail.com>
2025-12-23 20:46:29 +0100

    Turned on Firebase Analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,6 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
